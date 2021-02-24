518ef6dbd99c2e81243d1bb512c43473ed726f61
Joshua J. Haller <phone@joshuajhaller.com>
2021-02-24 10:39:45 -0800

    Added User component for future use. Printed user names into Body component to test data pulled.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Search from "./Search";
-import Card from "./Card";
+// import Card from "./Card";
 
-export default function Body() {
+export default function Body({ users }) {
   return (
     <div>
       <Search />
-      <Card />
+      <div className="card-body">
+        <h5 className="card-title">Employees</h5>
+        {users.map((user) => (
+          <p className="card-text">
+            {user.name.first}
+            {user.name.last}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
+
+//   //create component for image?
+//   {/* <div className="card" style={{ width: "18rem;" }}>
+// <img src={users((user) => (<p className="card-img-top"> {user.picture.thumbnail}</p>))} */}
+// //card component
+// {/* <Card /> */}
