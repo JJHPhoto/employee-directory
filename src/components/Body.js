11638545f303f5748de272ab35a97f849a5b4770
Joshua J. Haller <phone@joshuajhaller.com>
2021-02-24 09:52:02 -0800

    Added my Body component.

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "./Search";
+import Card from "./Card";
+
+export default function Body() {
+  return (
+    <div>
+      <Search />
+      <Card />
+    </div>
+  );
+}
