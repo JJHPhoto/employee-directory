1ed6d8a23f6b51e88e4a1d84ccd60f0bd7ada710
Joshua J. Haller <phone@joshuajhaller.com>
2021-03-04 12:44:47 -0800

    Clearer Naming conventions.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import SetStateContainer from "./components/SetState";
+import EmployeeDirectoryContainer from "./components/EmployeeDirectoryContainer";
 
 function App() {
-  return <SetStateContainer />;
+  return <EmployeeDirectoryContainer />;
 }
 
 export default App;
